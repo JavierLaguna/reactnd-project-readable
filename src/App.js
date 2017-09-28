be4facf03046a1c865adf0b3c11b0d28c7cdfa6a
Javier Laguna <javier.laguna92@gmail.com>
2017-09-28 17:01:19 +0200

    add redux store and app reducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react';
+import React, {PureComponent} from 'react';
 
-class App extends Component {
+export default class App extends PureComponent {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        hello world
       </div>
     );
   }
 }
-
-export default App;
