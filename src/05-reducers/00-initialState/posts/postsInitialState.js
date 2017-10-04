90e275fd5e4c863f25c862ef41b9fa99df0ba0fe
Javier Laguna <javier.laguna92@gmail.com>
2017-10-04 15:40:09 +0200

    add posts reducer

---
+++ src/05-reducers/00-initialState/posts/postsInitialState.js
@@ -0,0 +1,12 @@
+export default {
+  postsList: [{
+    id: 'id',
+    timestamp: Date.now(),
+    title: 'title',
+    body: 'body',
+    author: 'author',
+    category: 'category by gategories',
+    voteScore: 1,
+    deleted: false
+  }]
+}
---
+++ src/05-reducers/posts/postsReducer.js
@@ -0,0 +1,8 @@
+import appInitialState from '../00-initialState/app/appInitialState';
+
+export default function postsReducer(state = appInitialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
