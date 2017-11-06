ea3f80a5adf41815d21b0a22c885250eeaeaa02d
Javier Laguna <javier.laguna92@gmail.com>
2017-11-06 13:11:03 +0100

    edit comment flow

--- src/00-components/commentsList/index.js
+++ src/00-components/commentsList/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Comment from '../comment';
+import CommentEditable from '../commentEditable';
 
 export default class CommentsList extends Component {
 
@@ -8,7 +9,8 @@ export default class CommentsList extends Component {
     className: PropTypes.string,
     comments: PropTypes.array.isRequired,
     votePositive: PropTypes.func.isRequired,
-    voteNegative: PropTypes.func.isRequired
+    voteNegative: PropTypes.func.isRequired,
+    editComment: PropTypes.func.isRequired
   };
 
   static defaultProps = {
@@ -17,30 +19,59 @@ export default class CommentsList extends Component {
     votePositive: () => {
     },
     voteNegative: () => {
+    },
+    editComment: () => {
     }
   };
 
-  state = {};
+  state = {
+    commentEditable: ''
+  };
+
+  editComment(commentId) {
+    this.setState({commentEditable: commentId});
+  }
+
+  saveComment(comment, commentChanges) {
+    const editedComment = {
+      ...comment,
+      ...commentChanges
+    };
+    this.props.editComment(editedComment);
+    this.setState({commentEditable: ''});
+  }
 
   render() {
     const {className, comments, votePositive, voteNegative} = this.props;
-    
+    const {commentEditable} = this.state;
+
     return (
       <div className={className}>
-        {comments.map((comment, index) => (
-          <Comment key={index}
-                   id={comment.id}
-                   parentId={comment.parentId}
-                   voteScore={comment.voteScore}
-                   author={comment.author}
-                   deleted={comment.deleted}
-                   timestamp={comment.timestamp}
-                   body={comment.body}
-                   parentDeleted={comment.parentDeleted}
-                   voteNegative={voteNegative}
-                   votePositive={votePositive}
-          />
-        ))}
+        {comments.map((comment, index) => {
+          if (commentEditable === comment.id) {
+            return (
+              <CommentEditable className=''
+                               comment={comment}
+                               saveChanges={this.saveComment.bind(this, comment)}
+              />
+            )
+          }
+          return (
+            <Comment key={index}
+                     id={comment.id}
+                     parentId={comment.parentId}
+                     voteScore={comment.voteScore}
+                     author={comment.author}
+                     deleted={comment.deleted}
+                     timestamp={comment.timestamp}
+                     body={comment.body}
+                     parentDeleted={comment.parentDeleted}
+                     voteNegative={voteNegative}
+                     votePositive={votePositive}
+                     editComment={this.editComment.bind(this)}
+            />
+          )
+        })}
       </div>
     )
   }
